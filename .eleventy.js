834a644d5447f682952927af7c55c7989d8a7e7f
Jared White <jared@jaredwhite.com>
2023-09-11 17:14:00 -0700

    First batch of work on dashboard template

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/styles")
+  eleventyConfig.addPassthroughCopy("src/vendor")
+
+  eleventyConfig.addShortcode("component_dsd", function(name) {
+    const fileContents = fs.readFileSync(`src/_includes/components/${name}.html`)
+    return fileContents
+  })
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_layouts"
+    }
+  }
+}
